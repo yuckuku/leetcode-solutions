e042e9f0becc04325724026604f5be86b86377fd
yuckuku <yuckuku@163.com>
2017-10-17 13:39:02 +0800

    20171017

---
+++ src/main/java/InvertBinaryTree226.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Administrator on 2017/10/16.
+ */
+public class InvertBinaryTree226 {
+
+    public TreeNode invertTree(TreeNode root) {
+        if (null == root) {
+            return root;
+        }
+        TreeNode tn = root.left;
+        root.left = root.right;
+        root.right = tn;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
---
+++ src/main/java/LongestUncommonSubsequenceI.java
@@ -0,0 +1,41 @@
+/**
+ * Created by Administrator on 2017/10/10.
+ */
+public class LongestUncommonSubsequenceI {
+    public int findLUSlength(String a, String b) {
+        if (a.length() > b.length()) {
+            return a.length();
+        }
+        if (a.length() < b.length()) {
+            return b.length();
+        }
+        int usLen = -1;
+        Set<String> seta = findAllSubsequences (a);
+        for(String str: seta){
+            if(!b.contains(str)&& str.length()>usLen ){
+                usLen =str.length();
+            }
+        }
+        Set<String> setb = findAllSubsequences (b);
+        for(String str: setb){
+            if(!a.contains(str)&& str.length()>usLen ){
+                usLen =str.length();
+            }
+        }
+
+        return usLen ;
+    }
+
+    public Set<String> findAllSubsequences(String str){
+        if(null == str ){
+            return null;
+        }
+        Set<String> set = new HashSet<String>();
+        for(int len =0;len <=str.length();len++) {
+            for (int i = 0; i <=str.length() - len ; i++) {
+                set.add(str.substring(i, i + len));
+            }
+        }
+        return set;
+    }
+}
