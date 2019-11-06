fa74e267a26dabaeb04c951ca393a585a6e69a57
yuckuku <yuckuku@163.com>
2019-11-06 13:56:22 +0800

    20191106

--- docs/1111MaximumNestingDepthofTwoValidParenthesesStrings.md
+++ docs/1111MaximumNestingDepthofTwoValidParenthesesStrings.md
@@ -1,4 +1,12 @@
-
+* 找到A，然后后面填补1
+* 栈
+--------------
+* 区分子序列和字串之间的区别
+```
+给定 "pwwkew" ，
+子串是pww,wwk等很多个子串 是连在一起的
+子序列是 pwk,pke等很多个子序列 ，但是子序列中的字符在字符串中不一定是连在一起的。
+```
 
 ```
 A string is a valid parentheses string (denoted VPS) if and only if it consists of "(" and ")" characters only, and:
@@ -19,7 +27,8 @@ Given a VPS seq, split it into two disjoint subsequences A and B, such that A a
 
 Now choose any such A and B such that max(depth(A), depth(B)) is the minimum possible value.
 
-Return an answer array (of length seq.length) that encodes such a choice of A and B:  answer[i] = 0 if seq[i] is part of A, else answer[i] = 1.  Note that even though multiple answers may exist, you may return any of them.
+Return an answer array (of length seq.length) that encodes such a choice of A and B:  answer[i] = 0 if seq[i] is part of A, else answer[i] = 1. 
+Note that even though multiple answers may exist, you may return any of them.
 
  
 
--- src/main/java/leetcode_solutions/MaximumNestingDepthofTwoValidParenthesesStrings1111.java
+++ src/main/java/leetcode_solutions/MaximumNestingDepthofTwoValidParenthesesStrings1111.java
@@ -1,4 +1,111 @@
 package leetcode_solutions;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+
 public class MaximumNestingDepthofTwoValidParenthesesStrings1111 {
+
+    class Solution {
+        public int[] maxDepthAfterSplit(String seq) {
+            int[] re = new int[seq.length()];
+
+            Deque<Character> queue = new LinkedList<>();
+            queue.add(seq.charAt(0));
+            int i = 1;
+            for (; i < seq.length(); i++) {
+                if (seq.charAt(i) == ')') {
+                    queue.pop();
+                    if (queue.isEmpty()) {
+                        break;
+                    } else {
+                        re[i] = 1;
+                    }
+                } else {
+                    re[i] = 1;
+                    queue.add(seq.charAt(i));
+                }
+            }
+
+            i++;
+            for (; i < re.length; i++) {
+                re[i] = 1;
+            }
+
+
+            return re;
+        }
+    }
+
+    class Solution1 {
+        /**
+         * 遍历seq，遇到(则压入栈
+         * 遇到)则计算：
+         * 使用stack.size()获取当前深度
+         * 当前遍历stack.pop()和i 获取括号的左右下标
+         * depth%2 判断负责为1 还是0
+         */
+        public int[] maxDepthAfterSplit(String seq) {
+            if (seq == null || seq.isEmpty()) {
+                return new int[0];
+            }
+            int[] result = new int[seq.length()];
+            Stack<Integer> stack = new Stack<>();
+            for (int i = 0; i < seq.length(); i++) {
+                if (seq.charAt(i) == '(') {
+                    stack.push(i);
+                } else {
+                    int depth = stack.size();
+                    int left = stack.pop();
+                    if (depth % 2 == 0) {
+                        result[left] = 1;
+                        result[i] = 1;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+
+    class SolutionOnLeetcode1 {
+        public int[] maxDepthAfterSplit(String seq) {
+            int[] res = new int[seq.length()];
+            for (int i = 0, cnt = 0; i < res.length; ++i) {
+                if (seq.charAt(i) == '(') {
+                    res[i] = cnt++ & 1;
+                } else {
+                    res[i] = --cnt & 1;
+                }
+            }
+            return res;
+        }
+    }
+
+
+    @Test
+    public void test() {
+        String s = "()(())()";//[0,0,0,1,1,0,1,1]
+        Solution1 solution = new Solution1();
+        int[] re = solution.maxDepthAfterSplit(s);
+        System.out.println(Arrays.toString(re));
+        System.out.println("------------");
+
+        int cnt = 0;
+        System.out.println(cnt++ & 1);
+        System.out.println(cnt++ & 1);
+        System.out.println(cnt++ & 1);
+
+
+        System.out.println("-----------------------");
+        String s1 = "(((())))";
+        SolutionOnLeetcode1 solutionOnLeetcode1 = new SolutionOnLeetcode1();
+        int[] re1 = solutionOnLeetcode1.maxDepthAfterSplit(s1);
+
+        System.out.println(Arrays.toString(re1));
+        System.out.println(Arrays.toString(solution.maxDepthAfterSplit(s1)));
+    }
+
 }
